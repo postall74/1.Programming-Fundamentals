8a56845c2863fa7e4407eb7d7bd5b1dca73979bb
postall74 <postall_mcs@mail.ru>
2022-05-15 00:47:21 +0500

    Update Program.cs

--- 35.Dynamic array advanced/Dynamic array advanced/Program.cs
+++ 35.Dynamic array advanced/Dynamic array advanced/Program.cs
@@ -10,6 +10,50 @@ namespace Dynamic_array_advanced
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>();
+            string userInput = "";
+            bool isExit = false;
+
+            while (isExit == false)
+            {
+                Console.Clear();
+                Console.WriteLine("\nSum - получить сумму всех чисел в List<int>\nExit - выход из программы\n");
+
+                foreach (int number in list)
+                {
+                    Console.Write(number + " | ");
+                }
+                Console.WriteLine();
+                userInput = Console.ReadLine().ToLower();
+
+                if (userInput == "exit")
+                {
+                    isExit = true;
+                    Console.WriteLine("До встречи!");
+                }
+                else if (userInput == "sum")
+                {
+                    foreach (int number in list)
+                    {
+                        Console.Write(number + " | ");
+                    }
+                    Console.WriteLine($"\nСумма числе в List<int> - {list.Sum()}");
+                    Console.ReadKey(true);
+                }
+                else
+                {
+                    AddToList(list, userInput);
+                }
+            }
+        }
+
+        static void AddToList (List<int> list, string userInput)
+        {
+            int number;
+            if (int.TryParse(userInput, out number) == true)
+            {
+                list.Add(number);
+            }
         }
     }
 }
