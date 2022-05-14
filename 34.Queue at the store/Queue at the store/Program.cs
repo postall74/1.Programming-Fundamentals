b2d7f19b76972125dce7e626280cf0ef74b90c8b
postall74 <postall_mcs@mail.ru>
2022-05-14 22:27:04 +0500

    103491
    1. static Queue<int> FillShoppingQueue() { - вы не заполняете, а создаёте очередь. 2. for (int i = 0; i < array.Length; i++) { array[i] = random.Next(minimalSummShopping, maximalSummShopping); } for (int i = 0; i < array.Length; i++) { shoppingQueue.Enqueue(array[i]); } - зачем лишний массив? Сразу инициализируйте значение в очереди. 3. static bool CashBox(Queue<int> queue, ref int cashBox) - методы именуются с глагола в инфинитиве, указывая что они делают. 4. static bool CashBox(Queue<int> queue, ref int cashBox) название метода не отражает возвращение булевого значения

--- 34.Queue at the store/Queue at the store/Program.cs
+++ 34.Queue at the store/Queue at the store/Program.cs
@@ -9,11 +9,14 @@ namespace Queue_at_the_store
         {
             Console.CursorVisible = false;
             Queue<int> shoppingQueue = new Queue<int>();
+            Random random = new Random();
+            int maximalLengthQueue = 20;
+            int maximalSummShopping = 5000;
             int cashBox = 0;
-            shoppingQueue = FillShoppingQueue();
-            bool isExit = false;
+            FillShoppingQueue(shoppingQueue, maximalLengthQueue, maximalSummShopping, random);
+            bool isCloseCashBox = false;
 
-            while (isExit == false)
+            while (isCloseCashBox == false)
             {
                 Console.Clear();
                 Console.SetCursorPosition(0, 1);
@@ -25,41 +28,26 @@ namespace Queue_at_the_store
                 }
                 Console.WriteLine($"\nСледующая покупка будет на сумму - {shoppingQueue.Peek()}");
                 Console.SetCursorPosition(0, 0);
-                isExit = CashBox(shoppingQueue, ref cashBox);
+                PaymentForPurchase(shoppingQueue, ref cashBox, ref isCloseCashBox);
                 Console.ReadKey(true);
             }
         }
 
-        static Queue<int> FillShoppingQueue()
+        static void FillShoppingQueue(Queue<int> queue, int maximalLength, int maximalSummShopping, Random random)
         {
-            Queue<int> shoppingQueue = new Queue<int>();
-            int[] array;
-            Random random = new Random();
-            int minimalLengthQueue = 10;
-            int maximalLengthQueue = 20;
-            int minimalSummShopping = 100;
-            int maximalSummShopping = 5000;
-            array = new int[random.Next(minimalLengthQueue, maximalLengthQueue)];
-
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < random.Next(maximalLength); i++)
             {
-                array[i] = random.Next(minimalSummShopping, maximalSummShopping);
+                queue.Enqueue(random.Next(maximalSummShopping));
             }
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                shoppingQueue.Enqueue(array[i]);
-            }
-            return shoppingQueue;
         }
 
-        static bool CashBox(Queue<int> queue, ref int cashBox)
+        static void PaymentForPurchase(Queue<int> queue, ref int cashBox, ref bool isCloseCashBox)
         {
-            bool isExit = false;
+            isCloseCashBox = false;
 
             if (queue.Count == 1)
             {
-                isExit = true;
+                isCloseCashBox = true;
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.Write($"Денег в кассе - {cashBox}");
                 Console.ForegroundColor = ConsoleColor.White;
@@ -70,13 +58,12 @@ namespace Queue_at_the_store
             }
             else
             {
-                isExit = false;
+                isCloseCashBox = false;
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.Write($"Денег в кассе - {cashBox}");
                 Console.ForegroundColor = ConsoleColor.White;
                 cashBox += queue.Dequeue();
             }
-            return isExit;
         }
     }
 }
---
+++ 35.Dynamic array advanced/Dynamic array advanced/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_array_advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
