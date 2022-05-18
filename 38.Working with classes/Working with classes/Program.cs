ba0f2ebc408ffa439e2104eb2c06324bec3e04f5
postall74 <postall_mcs@mail.ru>
2022-05-19 02:05:31 +0500

    Create Program.cs

---
+++ 38.Working with classes/Working with classes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Working_with_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Jon", 100, 10, 25);
+            player.ShowInfoPlayer();
+        }
+    }
+
+    public class Player
+    {
+        private string _name;
+        private int _health;
+        private int _armor;
+        private int _damage;
+
+        public Player (string name, int health, int armaor, int damage)
+        {
+            _name = name;
+            _health = health;
+            _armor = armaor;
+            _damage = damage;
+        }
+
+        public void ShowInfoPlayer()
+        {
+            Console.WriteLine($"Имя персонажа - {_name}\n\t-\nПоказатели:\nЖизни - {_health}\nБроня - {_armor}\nАтака - {_damage}");
+        }
+    }
+}
