c9dac82d4a9ca0b86fb703c553576b723fae1234
postall_mcs <postall_mcs@mail.ru>
2022-05-24 01:40:02 +0500

    Update Program.cs

--- 43.Produse store/Produse store/Program.cs
+++ 43.Produse store/Produse store/Program.cs
@@ -11,6 +11,52 @@ namespace Produse_store
         static void Main(string[] args)
         {
             Buyer buyer = new Buyer();
+            Seller seller = new Seller
+            (
+                new List<Product>
+                {
+                    new Product("Колбаса", 400),
+                    new Product("Молоко", 90),
+                    new Product("Хлеб", 40),
+                    new Product("Яйца", 110),
+                    new Product("Мин.вода", 60)
+                }
+            );
+            bool isExit = false;
+
+            while (isExit == false)
+            {
+                Console.Clear();
+                Console.WriteLine($"Shop");
+                Console.SetCursorPosition(0, 6);
+                Console.WriteLine($"1.Show products in shop\n2.Show the items in the customer's cart\n" +
+                    $"3.Make a purchase\n4.Exit");
+                string userInput;
+                Console.SetCursorPosition(0, 2);
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        seller.Show();
+                        break;
+                    case "2":
+                        buyer.Show();
+                        break;
+                    case "3":
+                        seller.GiveProduct(buyer);
+                        break;
+                    case "4":
+                        isExit = true;
+                        Console.WriteLine($"Good bye!");
+                        break;
+                    default:
+                        Console.WriteLine($"Retype the command");
+                        break;
+                }
+                Console.ReadKey(true);
+
+            }
         }
     }
 
@@ -31,7 +77,6 @@ namespace Produse_store
         private List<Product> _products;
         public int Money { get; private set; }
 
-
         public Seller(List<Product> products)
         {
             Money = 0;
@@ -40,6 +85,7 @@ namespace Produse_store
 
         public void Show()
         {
+            Console.SetCursorPosition(0, 15);
             foreach (Product product in _products)
             {
                 if (_products.Count > 0)
@@ -50,19 +96,28 @@ namespace Produse_store
                 {
                     Console.WriteLine($"Products ran out");
                 }
+                Console.WriteLine();
             }
         }
 
-        public void TakeProduct(Buyer buyer)
+        public void GiveProduct(Buyer buyer)
         {
             Show();
             Console.Write($"Choose what you want to buy - ");
             string productName = Console.ReadLine();
+            Product soldProduct = _products.Find(product => product.Name == productName);
 
-            if (_products.Find(product => productName == product.Name).ToString() == productName)
+            if (soldProduct.Name == productName)
             {
-                Money += buyer.Purchase(productName);
-                _products.Remove(_products.Find(product => productName == product.Name));
+                if (buyer.Money >= soldProduct.Price)
+                {
+                    Money += buyer.TakeProduct(soldProduct);
+                    _products.Remove(soldProduct);
+                }
+                else
+                {
+                    Console.WriteLine($"You don't have enough funds");
+                }
             }
             else
             {
@@ -80,13 +135,16 @@ namespace Produse_store
         {
             Console.Write($"How much money do you have? - ");
             int money;
-            bool isNumber = int.TryParse(Console.ReadLine(), out money);
+            bool isNumber = false;
 
             while (isNumber == false)
             {
+                isNumber = int.TryParse(Console.ReadLine(), out money);
+
                 if (isNumber == true)
                 {
                     Money = money;
+                    _products = new List<Product>();
                 }
                 else
                 {
@@ -96,15 +154,34 @@ namespace Produse_store
                     isNumber = int.TryParse(Console.ReadLine(), out money);
                 }
             }
-            _products = new List<Product>();
         }
 
-        public int Purchase(string productName)
+        public int TakeProduct(Product product)
         {
-            int purchasePrice;
-            List<Product> products = _products;
-
+            int purchasePrice = product.Price; 
+            Money -= product.Price;
+            _products.Add(product);
             return purchasePrice;
+
+        }
+
+        public void Show()
+        {
+            Console.SetCursorPosition(0, 15);
+            Console.WriteLine($"Аmount of money - {Money}");
+
+            if (_products.Count > 0)
+            {
+                foreach (Product product in _products)
+                {
+                    Console.Write($"{product.Name}");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine($"Shopping cart is empty");
+            }
         }
     }
 }
